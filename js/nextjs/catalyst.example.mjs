fdd25df7d3192c30102584d5eaf953aded756cd0
Bill Liu <liu_bill@live.com>
2023-12-09 17:44:48 -0800

    [Next.JS] Move keys into its own file.

---
+++ js/nextjs/catalyst.example.mjs
@@ -0,0 +1,9 @@
+// Copy me, and remove the '.example' portion of the file name.
+
+const catalystOptions = {
+  systemName: 'catalyst-nextjs-example',
+  publicKey: 'YOUR PUBLIC KEY HERE',
+  privateKey: 'YOUR PRIVATE KEY HERE',
+  version: '0.0.1',
+}
+export default catalystOptions
--- js/nextjs/next.config.mjs
+++ js/nextjs/next.config.mjs
@@ -1,15 +1,13 @@
-import * as catalyst from '@catalyst-monitor/nextjs/config'
+import * as catalystConfig from '@catalyst-monitor/nextjs/config'
+import catalyst from './catalyst.mjs'
 
 /** @type {import('next').NextConfig} */
-export default catalyst.withCatalystConfig(
+export default catalystConfig.withCatalystConfig(
   {
+    // Strict Mode causes effects to be ran twice.
+    // Since Catalyst sometimes runs in effects, this causes
+    // double-reporting of events.
     reactStrictMode: false,
   },
-  {
-    systemName: 'catalyst-nextjs-example',
-    publicKey: 'UsHtftJZKdkF9WZ7DT4HSWQk08QHwZiUwEwab8wd',
-    privateKey: 'CqZNUYrUBaqcsacZCfSO/e4afBQ98WOqFdHQT7N6',
-    version: '0.0.1',
-    baseUrl: 'http://localhost:7070',
-  }
+  catalyst
 )
